391edb02fb2609b311e5cf29dcdeb69d54db40f2
obaturn <akintoyetoluwalase912@gmail.com>
2024-05-08 10:24:39 +0100

    adding my phase gate task

---
+++ gateOne/Day One/Java phase gate/TaskFifteen.java
@@ -0,0 +1,11 @@
+public class TaskFifteen {
+public static void main(String [] args){
+int i = 4 ;
+int result = 0;
+for(; i <= 5 ; i++){
+System.out.println(i);
+result = i * 5;
+}
+System.out.print(result);
+}
+}
