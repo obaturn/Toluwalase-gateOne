dc359c7ab90c899bd1f1a06a2d8bf834719a3850
obaturn <akintoyetoluwalase912@gmail.com>
2024-05-08 13:27:54 +0100

    adding files

---
+++ gateOne/Day Three/TaskOne.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.random;
+public class TaskOne {
+public static void main(String [] args){
+Scanner input = new Scanner(System.in);
+int questions = 0;
+int correct answers = 0;
+int randomNumber 1 = random.nextInt();
+int randomNumber 2 = random.nextInt();
+int randomMultiplication = randomNumber 1 * randomNumber 2;
+int random addition = random number 1 + randomNumber 2;
+int randomSubtraction = randomNumber 1 - randomNumber2;
+int random division = randomNumber 1 / randomNumber 2;
+for(int count = 0; count < 10 ; count ++){
+System.out.println("the question is what is" "+" randomNumber 1 "+" randomNumber 2");
+int Questions = input.nextInt();
+int correct answers += Questions ;
+}
+System.out.println("the correct answers is " + correct);
+}
+}
---
+++ gateOne/Day Three/phyton/taskone.py
@@ -0,0 +1,5 @@
+import random 
+def arithemetic problems(generate questions):
+	for words in range (10):
+	generate questions  = words
+ 
