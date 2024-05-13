b5199e7a9f432623be1e7f3bf0c2b8432a70708f
obaturn <akintoyetoluwalase912@gmail.com>
2024-05-13 17:03:23 +0100

    adding files

--- gateOne/Day Seven/java/MenstrationApp.java
+++ gateOne/Day Seven/java/MenstrationApp.java
@@ -1,15 +1,31 @@
 import java.util.Scanner;
-public class Menstration App {
-public static void main(String [] args){
+
+	public class Menstration App {
+
+	public static void main(String [] args){
+
 Scanner input = new Scanner(System.in);
-int cycleLength;
-int averageCycle;
-int currentDay;
+
+	int cycleLength;
+
+	int menstrationLength;
+
+	int currentDay;
+
 System.out.println("welcome to the  month of your menstration cycle");
-System.out.println("enter the last day you saw your mensration cycle last");
+
+System.out.println("enter the last day you saw your mensration cycle last in days ");
+
 cylcleLength = input.nextInt();
-System.out.println("Enter the average cylcle of your menstration")
-averageCylce = input.nextInt();
-System.out.println("Enter the current day of your menstration")
+
+System.out.println("Enter the average cylcle of your menstration");
+
+menstrationLength = input.nextInt();
+
+System.out.println("Enter the current day of your menstration");
+
 currentDay = input.nextInt();
-int nextMenstrationCycle = (currentDay + averageCycle);
+
+int nextMenstrationCycle = (currentDay + cycleLength -1) % cyclelength +1;
+  
+int flow date = nextMenstrationCycle - (cycleLength / 2) 
