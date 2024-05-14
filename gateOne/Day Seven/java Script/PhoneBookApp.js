27abf08f194c54311946241d87b7fcb700db09fc
obaturn <akintoyetoluwalase912@gmail.com>
2024-05-15 00:03:06 +0100

    adding a java Script task

---
+++ gateOne/Day Seven/java Script/PhoneBookApp.js
@@ -0,0 +1,11 @@
+const prompt =  required (prompt - sync)();
+const MyArray = ["firstNameArray" , "secondNameArray" , "phoneNumberArray"]
+String prompt = """
+1 -> Add Contact
+2 -> Remove Contact
+3 -> Find Contact By Phone Number
+4 -> Find Contact By Last Name
+5 -> Edit  Contact
+	""";
+console.log(prompt);
+
